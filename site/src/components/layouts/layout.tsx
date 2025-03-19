c7000951fed6ac28e6067fa2330a7297478723cd
Stavros <steveiliop56@gmail.com>
2025-03-19 22:20:29 +0200

    feat: add language selector

--- site/src/components/layouts/layout.tsx
+++ site/src/components/layouts/layout.tsx
@@ -1,12 +1,16 @@
 import { Center, Flex } from "@mantine/core";
 import { ReactNode } from "react";
+import { LanguageSelector } from "../language-selector/language-selector";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <Center style={{ minHeight: "100vh" }}>
-      <Flex direction="column" flex="1" maw={350}>
-        {children}
-      </Flex>
-    </Center>
+    <>
+      <LanguageSelector />
+      <Center style={{ minHeight: "100vh" }}>
+        <Flex direction="column" flex="1" maw={340}>
+          {children}
+        </Flex>
+      </Center>
+    </>
   );
 };
