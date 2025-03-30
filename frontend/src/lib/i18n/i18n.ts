e43bd651b6e36331154d1fc1fa556c069e80ebc1
Stavros <steveiliop56@gmail.com>
2025-03-30 16:10:42 +0300

    feat: load translations from cdn

--- frontend/src/lib/i18n/i18n.ts
+++ frontend/src/lib/i18n/i18n.ts
@@ -1,19 +1,37 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import resourcesToBackend from 'i18next-resources-to-backend';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import ChainedBackend from "i18next-chained-backend";
+import resourcesToBackend from "i18next-resources-to-backend";
+import HttpBackend from "i18next-http-backend";
 
 i18n
+  .use(ChainedBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .use(resourcesToBackend((language: string) => import(`./locales/${language}.json`)))
   .init({
-    fallbackLng: 'en',
-    debug: import.meta.env.MODE === 'development',
+    fallbackLng: "en",
+    debug: import.meta.env.MODE === "development",
 
     interpolation: {
       escapeValue: false,
-    }
+    },
+
+    load: "currentOnly",
+
+    backend: {
+      backends: [
+        HttpBackend,
+        resourcesToBackend(
+          (language: string) => import(`./locales/${language}.json`),
+        ),
+      ],
+      backendOptions: [
+        {
+          loadPath: "https://cdn.tinyauth.app/i18n/{{lng}}.json",
+        },
+      ],
+    },
   });
 
 export default i18n;
