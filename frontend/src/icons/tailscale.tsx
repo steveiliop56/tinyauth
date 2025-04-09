f68c580e11441daf467633ebddd2c59b5bca122b
Stavros <steveiliop56@gmail.com>
2025-04-09 17:36:59 +0300

    fix: fix tailscale icon color

--- frontend/src/icons/tailscale.tsx
+++ frontend/src/icons/tailscale.tsx
@@ -1,8 +1,8 @@
-import { useMantineColorScheme } from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
 import type { SVGProps } from "react";
 
 export function TailscaleIcon(props: SVGProps<SVGSVGElement>) {
-  const { colorScheme } = useMantineColorScheme();
+  const colorScheme = useColorScheme();
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
