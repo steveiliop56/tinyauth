f817b3b16c9a1a43ae768465f366dcf7841d5d06
ElevenNotes <github@11notes.ch>
2025-07-16 23:20:37 +0200

    feat: better health check and less log noise

--- internal/server/server.go
+++ internal/server/server.go
@@ -39,23 +39,18 @@ func logPath(path string) bool{
 }
 
 func NewServer(config types.ServerConfig, handlers *handlers.Handlers) (*Server, error) {
-	// Disable gin logs
 	gin.SetMode(gin.ReleaseMode)
 
-	// Create router and use zerolog for logs
 	log.Debug().Msg("Setting up router")
 	router := gin.New()
 	router.Use(zerolog())
 
-	// Read UI assets
 	log.Debug().Msg("Setting up assets")
 	dist, err := fs.Sub(assets.Assets, "dist")
-
 	if err != nil {
 		return nil, err
 	}
 
-	// Create file server
 	log.Debug().Msg("Setting up file server")
 	fileServer := http.FileServer(http.FS(dist))
 
@@ -63,43 +58,35 @@ func NewServer(config types.ServerConfig, handlers *handlers.Handlers) (*Server,
 	router.Use(func(c *gin.Context) {
 		// If not an API request, serve the UI
 		if !strings.HasPrefix(c.Request.URL.Path, "/api") {
-			// Check if the file exists
 			_, err := fs.Stat(dist, strings.TrimPrefix(c.Request.URL.Path, "/"))
-
-			// If the file doesn't exist, serve the index.html
 			if os.IsNotExist(err) {
 				c.Request.URL.Path = "/"
 			}
-
-			// Serve the file
 			fileServer.ServeHTTP(c.Writer, c.Request)
-
-			// Stop further processing
 			c.Abort()
 		}
 	})
 
 	// Proxy routes
-	router.GET("/api/auth/:proxy", handlers.AuthHandler)
+	router.GET("/api/auth/:proxy", handlers.ProxyHandler)
 
 	// Auth routes
 	router.POST("/api/login", handlers.LoginHandler)
-	router.POST("/api/totp", handlers.TotpHandler)
+	router.POST("/api/totp", handlers.TOTPHandler)
 	router.POST("/api/logout", handlers.LogoutHandler)
 
 	// Context routes
-	router.GET("/api/app", handlers.AppHandler)
-	router.GET("/api/user", handlers.UserHandler)
+	router.GET("/api/app", handlers.AppContextHandler)
+	router.GET("/api/user", handlers.UserContextHandler)
 
 	// OAuth routes
-	router.GET("/api/oauth/url/:provider", handlers.OauthUrlHandler)
-	router.GET("/api/oauth/callback/:provider", handlers.OauthCallbackHandler)
+	router.GET("/api/oauth/url/:provider", handlers.OAuthURLHandler)
+	router.GET("/api/oauth/callback/:provider", handlers.OAuthCallbackHandler)
 
 	// App routes
 	router.GET("/api/healthcheck", handlers.HealthcheckHandler)
 	router.HEAD("/api/healthcheck", handlers.HealthcheckHandler)
 
-	// Return the server
 	return &Server{
 		Config:   config,
 		Handlers: handlers,
@@ -108,9 +95,7 @@ func NewServer(config types.ServerConfig, handlers *handlers.Handlers) (*Server,
 }
 
 func (s *Server) Start() error {
-	// Run server
 	log.Info().Str("address", s.Config.Address).Int("port", s.Config.Port).Msg("Starting server")
-
 	return s.Router.Run(fmt.Sprintf("%s:%d", s.Config.Address, s.Config.Port))
 }
 
@@ -135,13 +120,15 @@ func zerolog() gin.HandlerFunc {
 		// Log request
 		if logPath(method + " " + path) {
 			switch {
-				case code >= 200 && code < 300:			
+				case code >= 200 && code < 300:
 					log.Info().Str("method", method).Str("path", path).Str("address", address).Int("status", code).Str("latency", latency).Msg("Request")
 				case code >= 300 && code < 400:
 					log.Warn().Str("method", method).Str("path", path).Str("address", address).Int("status", code).Str("latency", latency).Msg("Request")
 				case code >= 400:
 					log.Error().Str("method", method).Str("path", path).Str("address", address).Int("status", code).Str("latency", latency).Msg("Request")
 			}
-		}
+		}else{
+			log.Debug().Str("method", method).Str("path", path).Str("address", address).Int("status", code).Str("latency", latency).Msg("Request")
+		}		
 	}
 }
